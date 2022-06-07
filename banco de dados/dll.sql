4129d4c0dc25f3b822d32cfd6c5c5df332839946
AkamiAk <jvmdsofical24@gmail.com>
2022-06-07 11:40:11 -0300

    prova

---
+++ banco de dados/dll.sql
@@ -0,0 +1,9 @@
+create database SuperHeroi;
+use SuperHeroi;
+
+create table tb_super_heroi (
+	id_super_heroi    INT primary key,
+	nm_super_heroi       varchar(100),
+	ds_super_poder      varchar(200),
+	bt_voa          boolean
+);
